3fd36abd9fa73bbdb9116fccb76a82dd8bf77ff7
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-01-28 16:04:09 +0200

    Pass Test

--- 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/currency/EnglishConvertNumberToNameService.java
+++ 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/currency/EnglishConvertNumberToNameService.java
@@ -6,7 +6,23 @@ import org.springframework.stereotype.Service;
 public class EnglishConvertNumberToNameService {
 
     public String convertToString(int i) {
-        return NumNames.NUM_NAMES[i % 20];
+        String sofar = "";
+        if (i % 100 < 20) {
+            sofar += NumNames.NUM_NAMES[i % 20];
+            i /= 100;
+        } else {
+            sofar = NumNames.NUM_NAMES[i % 10];
+            i /= 10;
+
+            sofar = TensNames.TENS_NAMES[i % 10] + " " + sofar;
+            i /= 10;
+        }
+
+        if (i == 0) {
+            return sofar;
+        }
+
+        return (NumNames.NUM_NAMES[i] + " hundred " + sofar).trim();
     }
 
 }
