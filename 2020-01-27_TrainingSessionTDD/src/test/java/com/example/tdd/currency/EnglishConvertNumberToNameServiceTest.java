6ea0da1276afe168e25295e2fa79e4c3b5af47c4
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-01-28 13:40:44 +0200

    Write test

--- 2020-01-27_TrainingSessionTDD/src/test/java/com/example/tdd/currency/EnglishConvertNumberToNameServiceTest.java
+++ 2020-01-27_TrainingSessionTDD/src/test/java/com/example/tdd/currency/EnglishConvertNumberToNameServiceTest.java
@@ -1,19 +1,34 @@
 package com.example.tdd.currency;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnglishConvertNumberToNameServiceTest {
 
-    private EnglishConvertNumberToNameService englishConvertOneToNameService
+    private EnglishConvertNumberToNameService englishNumberNameService
             = new EnglishConvertNumberToNameService();
+
+    private final Map<Integer, String> otherNumbers = new HashMap<>();
+
+    @BeforeAll
+    public void init() {
+        otherNumbers.put(1, "one");
+        otherNumbers.put(2, "two");
+        otherNumbers.put(5, "five");
+        otherNumbers.put(9, "nine");
+        otherNumbers.put(18, "eighteen");
+        otherNumbers.put(20, "twenty");
+    }
 
     @Test
     public void shouldReturnValidStringAsNumber() {
-        String one = "one";
-
-        String result = englishConvertOneToNameService.convertToString(1);
-        Assertions.assertEquals(one, result);
+        for (int key : otherNumbers.keySet()) {
+            String result = englishNumberNameService.convertToString(key);
+            Assertions.assertEquals(otherNumbers.get(key), result);
+        }
     }
-
 }
