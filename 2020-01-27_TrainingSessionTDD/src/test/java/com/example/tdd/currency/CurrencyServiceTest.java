1f0f5c31e5e5284488e39faa521d226aa7775569
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-01-28 23:10:40 +0200

    Added first Mockito contract testing

--- 2020-01-27_TrainingSessionTDD/src/test/java/com/example/tdd/currency/CurrencyServiceTest.java
+++ 2020-01-27_TrainingSessionTDD/src/test/java/com/example/tdd/currency/CurrencyServiceTest.java
@@ -1,4 +1,49 @@
 package com.example.tdd.currency;
 
+import com.example.tdd.currency.entity.Currency;
+import com.example.tdd.currency.entity.CurrencyType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.util.Assert;
+
+//Contract testing
+@ExtendWith(MockitoExtension.class)
 public class CurrencyServiceTest {
+
+    @Mock
+    private static CurrencyRepository currencyRepository;
+
+    @Mock
+    private static EnglishConvertNumberToNameService englishConvertNumberToNameService;
+
+    @InjectMocks
+    private static CurrencyService currencyService;
+
+    @Test
+    public void shouldReturnCurrencyCreated() {
+        CurrencyType RAND_CURRENCY = CurrencyType.RAND;
+        String fifteenName = "fifteen";
+        int fifteenInt = 15;
+
+        Mockito
+                .when(englishConvertNumberToNameService.convertToString(Mockito.anyInt()))
+                .thenReturn(fifteenName);
+
+        Mockito
+                .lenient()
+                .when(currencyRepository.save(Mockito.any()))
+                .thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+
+        Currency currency = currencyService.save(RAND_CURRENCY, fifteenInt);
+
+        Assertions.assertEquals(RAND_CURRENCY, currency.getType());
+        Assertions.assertEquals(fifteenName, currency.getEnglishNumberName());
+        Assertions.assertEquals(fifteenInt, currency.getValue());
+    }
+
 }
