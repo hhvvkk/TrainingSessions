6d8cde6bd9ac15f854936d6f42eba8b789aed17a
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-01-28 13:59:41 +0200

    Pass test

---
+++ 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/currency/NumNames.java
@@ -0,0 +1,28 @@
+package com.example.tdd.currency;
+
+public class NumNames {
+
+    public static final String[] NUM_NAMES = {
+            "",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen"
+    };
+
+}
