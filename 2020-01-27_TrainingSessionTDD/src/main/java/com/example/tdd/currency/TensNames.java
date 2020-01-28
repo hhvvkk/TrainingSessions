e745b9efede6672743afc7a664e9fba7d2278b5d
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-01-28 16:02:38 +0200

    Added TensNames to test

---
+++ 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/currency/TensNames.java
@@ -0,0 +1,18 @@
+package com.example.tdd.currency;
+
+public class TensNames {
+
+    public static final String[] TENS_NAMES = {
+            "",
+            "ten",
+            "twenty",
+            "thirty",
+            "forty",
+            "fifty",
+            "sixty",
+            "seventy",
+            "eighty",
+            "ninety"
+    };
+
+}
