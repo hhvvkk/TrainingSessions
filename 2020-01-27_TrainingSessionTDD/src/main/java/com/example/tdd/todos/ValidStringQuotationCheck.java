71eb190439e6aa02c02e8a8afed9193b6f02aa28
Henko van Koesveld <henko.van.koesveld@advance.io>
2020-08-20 07:53:36 +0200

    Pass test

--- 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/todos/ValidStringQuotationCheck.java
+++ 2020-01-27_TrainingSessionTDD/src/main/java/com/example/tdd/todos/ValidStringQuotationCheck.java
@@ -12,13 +12,17 @@ package com.example.tdd.todos;
 public class ValidStringQuotationCheck {
     public boolean hasWellFormedQuotations(String stringToCheck) {
         int numberOfSingleQuotes = 0;
+        int numberOfQuotation = 0;
 
         for (char character: stringToCheck.toCharArray()) {
             if (character == '\'') {
                 numberOfSingleQuotes ++;
+            } else if (character == '\"') {
+                numberOfQuotation++;
             }
         }
 
-        return numberOfSingleQuotes % 2 == 0;
+        return numberOfSingleQuotes % 2 == 0
+                && numberOfQuotation % 2 == 0;
     }
 }
